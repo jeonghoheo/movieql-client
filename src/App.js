a84a20556a88212904b54b734c9653c7e0cecf81
jeonghoheo <heojeongho1992@gmail.com>
2019-08-18 20:21:25 +0900

    React Router사용하기
    Home과 Detail 컴포넌트를 간략하게 생성후 Router를 이용
    React.Fragment대신 <></>를 사용 같은 역할을 해준다.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
 import client from "./apolloClient";
 import { ApolloProvider } from "@apollo/react-hooks";
+import Home from "./Home";
+import Detail from "./Detail";
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="App" />
+      <Router>
+        <>
+          <Route exact={true} path={"/"} component={Home} />
+          <Route exact={true} path={"/detail/:movieId"} component={Detail} />
+        </>
+      </Router>
     </ApolloProvider>
   );
 }
