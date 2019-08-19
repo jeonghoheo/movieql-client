6e3c4493e57acb94d1a6dd2a4f0f725bed9a33ee
jeonghoheo <heojeongho1992@gmail.com>
2019-08-19 18:46:26 +0900

    useQuery에 인자 넘기기

--- src/Detail.js
+++ src/Detail.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { MOVIE_DETAILS } from "./queries";
 
 const Detail = props => {
   const {
@@ -6,7 +8,20 @@ const Detail = props => {
       params: { movieId }
     }
   } = props;
-  return <div>Detail</div>;
+
+  const { loading, error, data } = useQuery(MOVIE_DETAILS, {
+    variables: { movieId: parseInt(movieId) }
+  });
+  if (loading) return "loading";
+  if (error) return "error";
+  return (
+    <>
+      {data.movie.title}
+      {data.movie.medium_cover_image}
+      {data.movie.rating}
+      {data.movie.description_intro}
+    </>
+  );
 };
 
 export default Detail;
